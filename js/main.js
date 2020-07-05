7859ef99528758b054bd67cc4ab4e4c608f84016
stephanusyogi <stephanusyogi12@gmail.com>
2020-07-05 12:31:35 +0700

    testInagata

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// navbar
+function openNav() {
+          document.getElementById("mySidenav").style.width = "250px";
+        }
+        
+        function closeNav() {
+          document.getElementById("mySidenav").style.width = "0";
+        }
+
+// slider
